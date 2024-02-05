18a1fb21ade13d827ac6bd7e9a3c66ac9e51225b
= <xcrazy_starx@bk.ru>
2024-02-05 18:30:38 +0300

    123

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ from flask import Flask, jsonify
 import re
 
 app = Flask(__name__)
-def start(page_url: str):
-    parser = SMMParser()
-    html = parser.get(url=page_url)
-    return html
     # product_links = parser.get_product_links()
     # print(product_links)
     # for product_link in product_links:
@@ -19,7 +15,7 @@ def start(page_url: str):
     #     html = parser.get_page_html(product_url=product_link)
     #     parser.parse_page_html(html=html)
     
-    parser.quit_()
+    
 
 @app.route("/hello", methods=["GET"])
 def main():
@@ -30,9 +26,12 @@ def main():
     #                  "https://megamarket.ru/shop/tehnic/catalog/stroitelstvo-i-remont/",
     #                  "https://megamarket.ru/shop/tehnic/catalog/sport-i-aktivnyy-otdyh/"]
     page_urls = [f"https://megamarket.ru/shop/tehnic/catalog/elektronika/#?page={i}" for i in range(1, 19 + 1)]
-    #p = Pool(processes=1)
-    # p.map(start, page_urls)
-    start(page_urls[0])
+    parser = SMMParser()
+    html = parser.get(url=page_urls[0])
+    print(html)
+    parser.quit_()
+    return str(html)
+    
 
 
 if __name__ == "__main__":
--- selenium_driver/browser.py
+++ selenium_driver/browser.py
@@ -10,7 +10,7 @@ class Driver():
         options.add_argument("--window-size=1920,1080")
         options.add_argument('--ignore-certificate-errors')
         options.add_argument('--allow-running-insecure-content')
-        options.add_argument("--headless")
+        # options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument('--disable-dev-shm-usage')
         user_agent = 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
